63252db5985989d179f4739e22f611cb520061db
Thomas Helmkay <thomas.helmkay@e-healthinnovations.com>
2017-08-07 14:11:46 +0100

    added basic skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+links
+
+- http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+-
+  http://www.multigesture.net/articles/how-to-write-an-emulator-CHIP-8-interpreter/
+- http://mattmik.com/files/chip8/mastering/chip8.html
+- https://github.com/AfBu/haxe-CHIP-8-emulator/wiki/(Super)CHIP-8-Secrets
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+
+    //setup graphics
+    //setup input
+
+    //initialise the chip8
+
+    //emulation loop
+
+
+}
+
+
+struct Chip8{
+    memory:[u8;4096],
+    opcode:u16,//short opcode 2 byte
+    v:[u8;16],//registers 16 byte
+    index:u16,
+    pc:u16,
+    stack: [u16;16],
+    sp:u16
+        
+}
+
+
+impl Chip8{
+    pub fn new() -> Chip8{
+        //initialse the cpu core clear memory
+        return Chip8{memory:[0;4096]};
+    }
+
+    pub fn emulate_cyle(self){//might need to change reference on self
+        //fetch opcode
+        //decode opcode
+        //exeute opcode
+
+        //update timers
+    }
 }
